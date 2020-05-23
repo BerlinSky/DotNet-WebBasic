86b9eeed7eadd6158a0cefa467e7c651a3a35ca8
Pedro Young <pedroyoung@livenation.com>
2020-05-23 13:37:57 -0700

    Updates

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -25,21 +25,25 @@ const App = () => {
     setSelectedActivity(activity)                                  
     setEditMode(false)                                             
   }                                                                         
-                                                                   
+                                                                       
   const handleEditActivity = (activity: IActivity) => {                     
     setActivities([...activities.filter(a => a.id !== activity.id), activity])                                              
-    setSelectedActivity(activity)                                  
-    setEditMode(false)                                             
-  }                                                                         
+    setSelectedActivity(activity)                                      
+    setEditMode(false)                                                 
+  }                                                                    
+                                                                       
+  const handleDeleteActivity = (id: string) => {                       
+    setActivities([...activities.filter(a => a.id !== id)])            
+  }                                                                    
                                                                             
   useEffect(() => {                                                                
     axios.get<IActivity[]>('http://localhost:5000/api/activities/')                       
-      .then((response) => {                          
-        let activities: IActivity[] = []                          
-        response.data.forEach(activity => {          
-          activity.date = activity.date.split('.')[0]
-          activities.push(activity)            
-        })                                           
+      .then((response) => {                                            
+        let activities: IActivity[] = []                               
+        response.data.forEach(activity => {                            
+          activity.date = activity.date.split('.')[0]                  
+          activities.push(activity)                                    
+        })                                                             
         setActivities(activities)                                                      
       })                                                                                  
   }, []);                                                                                 
@@ -57,6 +61,7 @@ const App = () => {
             setSelectedActivity={setSelectedActivity}                   
             createActivity={handleCreateActivity}                       
             editActivity={handleEditActivity}                           
+            deleteActivity={handleDeleteActivity}                           
           />                                                                       
         </Container>                                                                      
       </Fragment>                                                                         
