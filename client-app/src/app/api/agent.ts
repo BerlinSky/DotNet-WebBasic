1a0f82928b691f88c4f493316ef68bc6aa71619d
Pedro Young <pedroyoung@livenation.com>
2020-05-23 20:50:53 -0700

    Updates

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,15 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';                 
-                                                                                
+                                                                                 
 axios.defaults.baseURL = 'http://localhost:5000/api';                                               
-                                                                                
-const responseBody = (response: AxiosResponse) => response.data;                
-                                                                                
-const requests = {                                                             
-  get: (url: string) => axios.get(url).then(responseBody),                      
-  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),    
-  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),              
-  del: (url: string) => axios.delete(url).then(responseBody),                           
+                                                                                 
+const responseBody = (response: AxiosResponse) => response.data;                 
+                                                                                 
+const sleep = (ms: number) => (response: AxiosResponse) =>                       
+  new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms));
+                                                                                 
+const requests = {                                                               
+  get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),                       
+  post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),     
+  put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),              
+  del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),                           
 }                                                                                       
                                                                                         
 const Activities = {                                                                    
